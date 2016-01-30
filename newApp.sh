07b6a043aceeafe4269be88eb56e360680dd6bfc
Rolando Lucio <rolandolucio@gmail.com>
2016-01-30 04:10:59 -0600

    0.0.1 ♥ saving time now!

--- newApp.sh
+++ newApp.sh
@@ -22,6 +22,9 @@ NEWAPP="$1"
 #base AIM path
 INSTALLDIR=/opt/bitnami
 
+#Apache app prefix to be updated later
+APACHEAPPSPREFIX=$INSTALLDIR/apache2/conf/bitnami/bitnami-apps-prefix.conf
+
 APPDIR=$INSTALLDIR/apps/$NEWAPP
 # Control app dirs.
 if [ -d "$APPDIR" ]; then
@@ -32,27 +35,31 @@ fi
 echo "Making App Folders"
 
 mkdir -p $APPDIR
-mkdir -p $INSTALLDIR/apps/$NEWAPP/htdocs
-mkdir -p $INSTALLDIR/apps/$NEWAPP/conf
-mkdir -p $INSTALLDIR/apps/$NEWAPP/data
 
-HTTPDPREFIX=$INSTALLDIR/apps/$NEWAPP/conf/httpd-prefix.conf
+APPHTDOCSDIR=$APPDIR/htdocs
+mkdir -p $APPHTDOCSDIR
+
+APPCONFDIR=$APPDIR/conf
+mkdir -p $APPCONFDIR
+
+APPDATADIR=$APPDIR/data
+mkdir -p $APPDATADIR
+
+HTTPDPREFIX=$APPCONFDIR/httpd-prefix.conf
+HTTPDAPP=$APPCONFDIR/httpd-app.conf
 
 echo "Writing: $HTTPDPREFIX"
 
 cat > $HTTPDPREFIX <<EOL
-Alias /${NEWAPP}/ "${INSTALLDIR}/apps/${NEWAPP}/htdocs/"
-Alias /${NEWAPP} "${INSTALLDIR}/apps/${NEWAPP}/htdocs"
-Include "${INSTALLDIR}/apps/${NEWAPP}/conf/httpd-app.conf"
+Alias /${NEWAPP}/ "${APPHTDOCSDIR}/"
+Alias /${NEWAPP} "${APPHTDOCSDIR}"
+Include "${HTTPDAPP}"
 EOL
-
-
-HTTPDAPP=$INSTALLDIR/apps/$NEWAPP/conf/httpd-app.conf
 
 echo "Writing: $HTTPDAPP"
 
 cat > $HTTPDAPP <<EOL
-<Directory ${INSTALLDIR}/apps/${NEWAPP}/htdocs>
+<Directory ${APPHTDOCSDIR}>
     Options +FollowSymLinks
     AllowOverride None
     <IfVersion < 2.3 >
@@ -64,3 +71,36 @@ cat > $HTTPDAPP <<EOL
     </IfVersion>
 </Directory>
 EOL
+
+echo "Appending: $APACHEAPPSPREFIX"
+
+cat >> $APACHEAPPSPREFIX <<EOL
+Include "${HTTPDPREFIX}"
+EOL
+
+echo "Adding index.php"
+
+cat > $APPHTDOCSDIR/index.php <<EOL
+<html>
+<body>
+<h1>${NEWAPP} is ready to go!!</h1>
+<p>Hope this little script relieved you.</p>
+<p>Regards,<br><a href="https://github.com/elkusko">Rolando Lucio</a></p>
+<a href="https://github.com/elkusko/bitnami-scripts"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
+</body
+</html>
+EOL
+
+echo "Changing ownership $APPHTDOCSDIR"
+chown -R bitnami:daemon $APPHTDOCSDIR
+
+echo "Updating permissions $APPHTDOCSDIR"
+find $APPHTDOCSDIR/ -type f -exec chmod 664 {} \;
+find $APPHTDOCSDIR/ -type d -exec chmod 755 {} \;
+
+
+echo "Restart Apache"
+$INSTALLDIR/ctlscript.sh restart apache
+
+
+echo "Custom PHP Application Added: $NEWAPP"
